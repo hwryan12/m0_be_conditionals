6cc02ec63211644a57bcf46ea218d5d61cc8bfb0
Harrison Ryan <hwryan12@gmail.com>
2022-11-02 13:09:52 -0500

    Finished if_statements. rb

--- if_statements.rb
+++ if_statements.rb
@@ -26,7 +26,19 @@ end
 
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'coat'
+weather = "icy"
 
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
 
 ##################
 # Using the num_quarters variable defined below, determine
@@ -41,10 +53,12 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
-
+num_quarters = 3
+if num_quarters >= 2
 puts "I have enough money for a gumball"
+else
 puts "I don't have enough money for a gumball"
+end
 
 
 #####################
@@ -61,5 +75,14 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce == true
+  puts "I can make pizza"
+else
+  cups_of_flour < 2 || has_sauce == false
+  puts "I cannot make pizza"
+
+end
+
